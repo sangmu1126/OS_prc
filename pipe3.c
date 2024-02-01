58328c9aac99e7f80167d5de55711364d8d9d200
paka99 <sangmu1126@gmail.com>
2024-01-31 23:04:20 -0800

    OS practice

--- pipe3.c
+++ pipe3.c
@@ -5,15 +5,15 @@
 #include <limits.h>
 
 int count;
-// void alrm_action(int);
+void alrm_action(int);
 
 void main() {
 	int p[2];
 	int pipe_size;
 	char c = 'X';
-//	static struct sigaction act;
-//	act.sa_handler = alrm_action;
-//	sigfillset(&(act.sa_mask));
+	static struct sigaction act;
+	act.sa_handler = alrm_action;
+	sigfillset(&(act.sa_mask));
 
 	if (pipe(p) ==-1){
 		perror("pipe call");
@@ -23,22 +23,22 @@ void main() {
 	pipe_size = fpathconf(p[0], _PC_PIPE_BUF);
 	printf("Maximum size of write to pipe : %d bytes \n", pipe_size);
 
-//	sigaction(SIGALRM, &act, NULL) // work if alarm signal done
+	sigaction(SIGALRM, &act, NULL); // work if alarm signal done
 
 	while(1) {
-//		alarm(20);
+		alarm(20);
 		write(p[1], &c, 1);
-//		alarm(0);
+		alarm(0);
 
 		if ((++count % 1024) == 0)
 			printf("%d characters in pipe \n", count);
 
 		if (pipe_size==count)
-			break;
+			alarm(1);
 	}
 }
 
-//void alrm_action(int signo){
-//	printf("write blocked after %d aharacters \n", count);
-//	exit(0);
-//}
+void alrm_action(int signo){
+	printf("write blocked after %d characters \n", count);
+	exit(0);
+}
